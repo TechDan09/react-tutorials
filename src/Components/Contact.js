25c3751a292a2fcee2c027735fdc38545ed18662
techdan09 <techdan09@gmail.com>
2019-07-05 16:42:38 +0100

    worked on contact page

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -4,23 +4,34 @@ import Sidebar from './Sidebar';
 function Contact() {
 	return (
 		<div class="container mt5">
-		  <h2>Stacked form</h2>
-		  <form action="/action_page.php">
-		    <div class="form-group">
-		      <label for="email">Email:</label>
-		      <input type="email" class="form-control" id="email" placeholder="Enter email" name="email" />
-		    </div>
-		    <div class="form-group">
-		      <label for="pwd">Password:</label>
-		      <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pswd" />
-		    </div>
-		    <div class="form-group form-check">
-		      <label class="form-check-label">
-		        <input class="form-check-input" type="checkbox" name="remember" /> Remember me
-		      </label>
-		    </div>
-		    <button type="submit" class="btn btn-primary">Submit</button>
-		  </form>
+			<div class="row">
+			  	<div class="col-md-8">
+			  		{/*<div class="row">*/}
+				  		<h2>Stacked form</h2>
+				  		<form action="/action_page.php">
+					    	<div class="form-group">
+					      		<label for="email">Email:</label>
+					      		<input type="email" class="form-control" id="email" placeholder="Enter email" name="email" />
+					    	</div>
+					    	<div class="form-group">
+					      		<label for="pwd">Password:</label>
+					      		<input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pswd" />
+					    	</div>
+					    	<div class="form-group form-check">
+					      		<label class="form-check-label">
+					        		<input class="form-check-input" type="checkbox" name="remember" /> Remember me
+					      		</label>
+					    	</div>
+					    	<button type="submit" class="btn btn-primary">Submit</button>
+				  		</form>
+					{/*</div>*/}
+				</div>
+				  	<div class="col-md-4">
+				  		<div class="row">
+							<Sidebar />
+						</div>
+					</div>
+			</div>
 		</div>
 	)
 }
